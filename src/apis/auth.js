fd37235e8657724fe0342721dce3a9ac034d85bd
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-02-15 01:32:53 +0900

    refactor : API 함수 내 에러 처리 부분 삭제

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -3,21 +3,9 @@ import { instance } from './api';
 const AUTH_URL = 'auth';
 
 export const signUpApi = async (email, password) => {
-  try {
-    const response = await instance.post(`${AUTH_URL}/signup`, { email, password });
-
-    return response;
-  } catch (error) {
-    throw error;
-  }
+  return instance.post(`${AUTH_URL}/signup`, { email, password });
 };
 
 export const signInApi = async (email, password) => {
-  try {
-    const response = await instance.post(`${AUTH_URL}/signin`, { email, password });
-
-    return response;
-  } catch (error) {
-    throw error;
-  }
+  return instance.post(`${AUTH_URL}/signin`, { email, password });
 };
--- src/apis/todo.js
+++ src/apis/todo.js
@@ -3,41 +3,17 @@ import { instance } from './api';
 const TODO_URL = 'todos';
 
 export const createTodoApi = async todo => {
-  try {
-    const response = await instance.post(TODO_URL, { todo });
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  return instance.post(TODO_URL, { todo });
 };
 
 export const getTodosApi = async () => {
-  try {
-    const response = await instance.get(TODO_URL);
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  return instance.get(TODO_URL);
 };
 
 export const updateTodoApi = async (id, todo, isCompleted) => {
-  try {
-    const response = await instance.put(`${TODO_URL}/${id}`, { todo, isCompleted });
-
-    return response;
-  } catch (error) {
-    throw error;
-  }
+  return instance.put(`${TODO_URL}/${id}`, { todo, isCompleted });
 };
 
 export const deleteTodoApi = async id => {
-  try {
-    const response = await instance.delete(`${TODO_URL}/${id}`);
-
-    return response;
-  } catch (error) {
-    throw error;
-  }
+  return instance.delete(`${TODO_URL}/${id}`);
 };
