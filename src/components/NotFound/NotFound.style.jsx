78d922de663342d8c7f9e8f77f74a3cf12e305be
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-02-15 01:36:55 +0900

    design : 404 페이지 마크업 및 스타일링

---
+++ src/components/NotFound/NotFound.style.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const NotFoundWrapper = styled.em`
+  color: var(--primary);
+  font-size: 1.4rem;
+`;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,13 @@
 import { MainContainerWrapper } from '../components/common/MainContainer/MainContainer.style';
+import NotFound from '../components/NotFound/NotFound';
 
 const NotFoundPage = () => {
-  return <MainContainerWrapper>NotFoundPage</MainContainerWrapper>;
+  return (
+    <MainContainerWrapper>
+      <h1>404</h1>
+      <NotFound />
+    </MainContainerWrapper>
+  );
 };
 
 export default NotFoundPage;
