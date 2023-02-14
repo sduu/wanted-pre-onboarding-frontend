a5491df2df00194b4c230c56101eb35edf3ed984
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-02-15 01:29:46 +0900

    feat : 투두 리스트 수정 기능 구현

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,10 +1,17 @@
 import { useRef, useState, useEffect } from 'react';
 import { Button, Label, Span, Input, TodoItemWrapper } from './TodoItem.style';
+import { useTodoContext } from '../../context/todoContext';
+import { updateTodoApi } from '../../apis/todo';
 import InputItem from '../common/InputItem/InputItem';
 import useInput from '../../hooks/useInput';
+import useApi from '../../hooks/useApi';
 
 const TodoItem = ({ todoData }) => {
-  const { todo, isCompleted } = todoData;
+  const { id, todo, isCompleted } = todoData;
+
+  const { updateTodo } = useTodoContext();
+
+  const [{ data: updatedTodo }, callUpdateTodoApi] = useApi(updateTodoApi, [], true);
 
   const [isEdit, setIsEdit] = useState(false);
   const textInputRef = useRef();
@@ -15,22 +22,42 @@ const TodoItem = ({ todoData }) => {
     setIsEdit(prev => !prev);
   };
 
+  const onSubmitHandler = async e => {
+    e.preventDefault();
+
+    console.log(updatedTodo);
+    const response = await callUpdateTodoApi(id, textValue, isChecked);
+
+    if (response) {
+      updateTodo({ id, todo: textValue, isCompleted: isChecked });
+      toggleIsEdit();
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       textInputRef.current.focus();
     }
   }, [isEdit]);
 
+  useEffect(() => {
+    callUpdateTodoApi(id, textValue, isChecked);
+  }, [isChecked]);
+
   return (
     <TodoItemWrapper>
       {isEdit ? (
         <>
-          <Input type='checkbox' onChange={toggleIsChecked} checked={isChecked} />
-          <InputItem type='text' value={textValue} onChange={updateTextValue} id='modify' inputRef={textInputRef} />
-          <Button data-testid='submit-button'>제출</Button>
-          <Button onClick={toggleIsEdit} data-testid='cancel-button'>
-            취소
-          </Button>
+          <TodoItemWrapper as='form' onSubmit={onSubmitHandler}>
+            <Input type='checkbox' onChange={toggleIsChecked} checked={isChecked} />
+            <InputItem type='text' value={textValue} onChange={updateTextValue} id='modify' inputRef={textInputRef} />
+            <Button type='submit' data-testid='submit-button'>
+              제출
+            </Button>
+            <Button onClick={toggleIsEdit} data-testid='cancel-button'>
+              취소
+            </Button>
+          </TodoItemWrapper>
         </>
       ) : (
         <>
